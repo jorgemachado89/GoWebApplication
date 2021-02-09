03526fb9df3f77bd5e4e68d542fd43d2dced97e4
jorgemachado89 <jorgemachado89@gmail.com>
2021-02-09 16:42:24 +0000

    Use of builtin function len for determining length of array

--- server/src/main/main.go
+++ server/src/main/main.go
@@ -10,6 +10,7 @@ const tax = 6.75 / 100
 type Product struct {
 	Name  string
 	Price float32
+	SomeArray []string
 }
 
 func (p Product) PriceWithTax() float32 {
@@ -21,12 +22,14 @@ const templateString = `
 Name: {{ .Name }}
 Price: {{ printf "$%.2f" .Price }}
 Price with Tax: {{ .PriceWithTax | printf "$%.2f" }}
+Length: {{ len .SomeArray }}
 `
 
 func main() {
 	p := Product{
 		Name:  "Lemonade",
 		Price: 2.16,
+		SomeArray: []string{"a", "b"},
 	}
 	t := template.Must(template.New("").Parse(templateString))
 	t.Execute(os.Stdout, p)
