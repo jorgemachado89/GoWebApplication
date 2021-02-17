2ac836336f6ac2c0877dc341414354a60fe7cb76
jorgemachado89 <jorgemachado89@gmail.com>
2021-02-17 18:07:26 +0000

    Added examples on how to deal with simple form data for authentication purposes

--- server/src/main/controller/home.go
+++ server/src/main/controller/home.go
@@ -4,17 +4,21 @@ import (
 	"html/template"
 	"net/http"
 	"main/viewmodel"
+	"fmt"
+	"log"
 )
 
 type home struct {
-	homeTemplate         *template.Template
-	standLocatorTemplate *template.Template
+	homeTemplate			*template.Template
+	standLocatorTemplate	*template.Template
+	loginTemplate			*template.Template
 }
 
 func (h home) registerRoutes() {
 	http.HandleFunc("/", h.handleHome)
 	http.HandleFunc("/home", h.handleHome)
 	http.HandleFunc("/stand-locator", h.handleStandLocator)
+	http.HandleFunc("/login", h.handleLogin)
 }
 
 func (h home) handleHome(w http.ResponseWriter, r *http.Request) {
@@ -25,4 +29,28 @@ func (h home) handleHome(w http.ResponseWriter, r *http.Request) {
 func (h home) handleStandLocator(w http.ResponseWriter, r *http.Request) {
 	vm := viewmodel.NewStandLocator()
 	h.standLocatorTemplate.Execute(w, vm)
+}
+
+func (h home) handleLogin(w http.ResponseWriter, r *http.Request) {
+	vm := viewmodel.NewLogin();
+	fmt.Printf("Start")
+	if r.Method == http.MethodPost {
+		err := r.ParseForm();
+		if err != nil {
+			log.Println(fmt.Errorf("Error logging in %v: ", err));
+		}
+		email := r.Form.Get("email")
+		password := r.Form.Get("password")
+		if (email == "test@gmail.com" && password == "password") {
+			http.Redirect(w, r, "/home", http.StatusTemporaryRedirect)
+			return
+		} else {
+			vm.Email = email
+			vm.Password = password
+		}
+		// fmt.Printf("Initial Form: %v \n", r.Form)
+		// fmt.Printf("Username: %v \n", r.FormValue("email"))
+		// fmt.Printf("Form: %v \n", r.Form)
+	}
+	h.loginTemplate.Execute(w, vm);
 }
